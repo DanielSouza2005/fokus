2e5080024f0b466ad8dba0f71d55adb9da473c06
DanielSouza2005 <danisimoes741@gmail.com>
2026-04-20 21:59:30 -0300

    refactor index screen layout; add new assets and remove unused images

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,68 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.jsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require("../assets/images/pomodoro.png")}></Image>
+
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 40,
+  },
+  actions: {
+    padding: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
+    color: "#fff",
+    fontweight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#BB72FF",
+    padding: 8,
+    borderRadius: 32,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+  },
+  footer: {
+    width: "80%",
+  },
+  footerText: {
+    color: "#98A0A8",
+    textAlign: "center",
+    fontSize: 12.5,
+  },
+});
